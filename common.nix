efb352081a7af4d7523297b1cf13c7dc41ceb18d
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2026-09-10 11:20:05 -0300

    add: additional locales

--- common.nix
+++ common.nix
@@ -32,6 +32,12 @@
     LC_TIME = "es_UY.UTF-8";
   };
 
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "es_UY.UTF-8/UTF-8"
+    "en_GB.UTF-8/UTF-8"
+  ];
+
   services = {
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
