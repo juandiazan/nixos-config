ba57ae5aaf40ca5246c1d2873b62855523f1d6e8
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2026-09-22 23:13:34 -0300

    ref: change aliases

--- modules/shell/aliases.nix
+++ modules/shell/aliases.nix
@@ -2,12 +2,11 @@
   home.shellAliases = {
     lzg = "lazygit";
     lzd = "lazydocker";
-    cdot = "cd dotfiles";
-    codhere = "codium .";
     rebuild = "sudo nixos-rebuild switch -L --flake ~/nixos-config";
     update = "sudo nix flake update --flake ~/nixos-config";
     enc = "tmux new-session -A -s nixos-config -c ~/nixos-config nvim .";
     rip = "abcde";
+    devinit = "devenv init --include-envrc";
     "ls-status" = "~/nixos-config/scripts/network/localsend-toggle.sh status";
     "ls-on" = "~/nixos-config/scripts/network/localsend-toggle.sh on";
     "ls-off" = "~/nixos-config/scripts/network/localsend-toggle.sh off";
